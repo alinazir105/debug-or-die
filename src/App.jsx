18c4674925566384553685a868164b1065b3e10a
alinazir105 <alinazir105@gmail.com>
2025-01-08 16:14:06 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './index.css'
+
+export default function App() {
+  return (
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+          <h1 className="text-4xl font-bold text-blue-600">
+              Hello, Debug or Die!
+          </h1>
+      </div>
+  );
+}
