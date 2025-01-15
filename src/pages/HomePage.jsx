7b0620a771ea036e79bb814f5ced8b2460ebc650
Muhammad Hammad <muhammadhammad52@gmail.com>
2025-01-15 18:01:50 +0500

    timer style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,24 @@ import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 class Example extends Component {
   render() {
-    return <FlipClockCountdown to={new Date().getTime() + 24 * 1000 + 5000} />;
+    return (
+      <FlipClockCountdown
+        to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+        renderMap={[false, false, true, true]}
+        labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+        labelStyle={{
+          fontSize: 10,
+          fontWeight: 500,
+          textTransform: "uppercase",
+        }}
+        digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
+        dividerStyle={{ color: "white", height: 1 }}
+        separatorStyle={{ color: "red", size: "6px" }}
+        duration={0.5}
+      >
+        Finished
+      </FlipClockCountdown>
+    );
   }
 }
 
